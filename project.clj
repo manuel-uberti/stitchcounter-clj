ed85bf2ace12a06c606dd4cd8f139397be64dc19
Manuel Uberti <manuel@boccaperta.com>
2015-05-12 14:41:37 +0200

    Add squiggly-clojure for Flycheck

--- project.clj
+++ project.clj
@@ -3,6 +3,12 @@
   :url "http://github.com/manuel-uberti/stitchcounter-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [acyclic/squiggly-clojure "0.1.2-SNAPSHOT"]]
   :main stitchcounter-clj.stitchcounter
-  :bin {:name "stitch-counter"})
+  :bin {:name "stitch-counter"}
+  :plugins [[lein-environ "1.0.0"]]
+
+  ;; Configuration here may be overridden by namespace metadata.
+  :profiles {:dev {:env {:squiggly {:checkers [:eastwood]
+                                    :eastwood-exclude-linters [:unlimited-use]}}}})
